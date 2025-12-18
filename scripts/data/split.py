1cd2d9aec81fe72a59880150603d16e37d8dc497
Savannah Ferretti <savannah.ferretti@uci.edu>
2025-12-17 19:47:41 -0600

    Update split.py

--- scripts/data/split.py
+++ scripts/data/split.py
@@ -10,20 +10,18 @@ logger = logging.getLogger(__name__)
 warnings.filterwarnings('ignore')
 
 if __name__=='__main__':
-    config = Config()
+    config   = Config()
     splitter = DataSplitter(
         filedir=config.interimdir,
         savedir=config.splitsdir,
         trainrange=config.trainrange,
         validrange=config.validrange,
         testrange=config.testrange)
-
     logger.info('Setting up splits...')
     splits = [
         ('train',splitter.trainrange),
         ('valid',splitter.validrange),
         ('test',splitter.testrange)]
-
     logger.info('Creating and saving normalized data splits...')
     trainstats = None
     for splitname,splitrange in splits:
